125dae37b0fc3be36ae747cd9e1ff3af7f876447
Fengxiu Zhang <shown05807@gmail.com>
2019-08-14 11:36:57 -0700

    inrease time

--- Agave_Experiment/Agave_1to25Reps.R
+++ Agave_Experiment/Agave_1to25Reps.R
@@ -18,7 +18,7 @@ experiment <- nl_experiment(
   iterations =1000,
   
   param_values = nl_param_oat(
-    n=20,
+    n=10,
     meanRIskThreshold = 0.4,
     scanningRange = 4,
     numWindows = c(0,6,10),
