a3c15006d9b24a8eeeb36cd73c1ab5733c9fff23
Fengxiu Zhang <shown05807@gmail.com>
2019-08-15 15:39:22 -0700

    D

---
+++ Agave_Experiment/run_sbatch_Parallel_76to100TEST.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+#SBATCH --cpus-per-task=15
+#SBATCH -t 0-12:00                  # wall time (D-HH:MM)
+##SBATCH -A fzhang59                 # Account hours will be pulled from (commented out with double # in front)
+#SBATCH -o slurm.%j.out             # STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err             # STDERR (%j = JobId)
+#SBATCH --mail-type=ALL             # Send a notification when the job starts, stops, or fails
+#SBATCH --mail-user=fzhang59@asu.edu # send-to address
+
+module load rstudio/1.1.423
+srun Rscript Agave_76to100RepsTEST.R
+date
