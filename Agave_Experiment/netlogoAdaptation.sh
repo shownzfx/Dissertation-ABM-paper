3beaaf4882a83fbe5cd5de1d9d26f96c11d25e47
Fengxiu Zhang <shown05807@gmail.com>
2020-01-17 15:15:38 -0700

    Update netlogoAdaptation.sh

--- Agave_Experiment/netlogoAdaptation.sh
+++ Agave_Experiment/netlogoAdaptation.sh
@@ -16,7 +16,7 @@
 bash /home/fzhang59/NetLogo\ 6.0.2/netlogo-headless.sh \
 --model Dissertation_ABM_update2020_cleaner.nlogo \
 --experiment adaptation \
-#--table ExpResults2400Runs.csv \
+--table ExpResults240Runs.csv \
 --threads 28
 
 
