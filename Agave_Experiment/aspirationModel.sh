f25f486c1af9fa789d0b30da4020068a65d3026f
Fengxiu Zhang <shown05807@gmail.com>
2020-01-17 15:46:49 -0700

    d

--- Agave_Experiment/aspirationModel.sh
+++ Agave_Experiment/aspirationModel.sh
@@ -16,7 +16,7 @@
 bash /home/fzhang59/NetLogo\ 6.0.2/netlogo-headless.sh \
 --model Dissertation_ABM_update2020_cleaner.nlogo \
 --experiment aspirationOn \
---table ExpResults240Runs.csv \
+--table ExpResults2160Runs.csv \
 --threads 28
 
 
