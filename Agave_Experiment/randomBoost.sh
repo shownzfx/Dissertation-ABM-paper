0d433ec21c0012f38729c2e954b546d12e2f700a
Fengxiu Zhang <shown05807@gmail.com>
2020-02-15 23:48:04 -0700

    d

--- Agave_Experiment/randomBoost.sh
+++ Agave_Experiment/randomBoost.sh
@@ -16,7 +16,7 @@
 bash /home/fzhang59/NetLogo\ 6.0.2/netlogo-headless.sh \
 --model Dissertation_ABM_update2020_cleaner.nlogo \
 --experiment randomBoost \
---table randomBoost200Runs.csv \
+--table randomBoost300Runs.csv \
 --threads 28
 
 
