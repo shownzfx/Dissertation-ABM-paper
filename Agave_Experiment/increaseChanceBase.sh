79b818dcee71c9c36256d53141ad1404cd07fad2
Fengxiu Zhang <shown05807@gmail.com>
2020-03-13 00:44:00 -0700

    d

--- Agave_Experiment/increaseChanceBase.sh
+++ Agave_Experiment/increaseChanceBase.sh
@@ -15,7 +15,7 @@
 
 bash /home/fzhang59/NetLogo\ 6.0.2/netlogo-headless.sh \
 --model Dissertation_ABM_update2020_cleaner.nlogo \
---experiment increaseChanceBase.sh \
+--experiment increaseChanceBase \
 --table increaseChanceBase100Runs_032020.csv \
 --threads 28
 
