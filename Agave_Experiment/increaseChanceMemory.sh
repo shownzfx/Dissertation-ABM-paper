94493175a5af2f8ea347323f3615b81a6f291e40
Fengxiu Zhang <shown05807@gmail.com>
2020-03-28 21:41:43 -0700

    d

--- Agave_Experiment/increaseChanceMemory.sh
+++ Agave_Experiment/increaseChanceMemory.sh
@@ -16,7 +16,7 @@
 bash /home/fzhang59/NetLogo\ 6.0.2/netlogo-headless.sh \
 --model Dissertation_ABM_update2020_cleaner.nlogo \
 --experiment inceaseChanceMemory \
---table increaseChanceMemory900Runs_032020.csv \
+--table increaseChanceMemory800Runs_032020.csv \
 --threads 28
 
 
